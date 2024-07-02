78f9d9a796c2ff1eaca9d1a8f2b7f7dd1f7206c2
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-07-02 22:50:47 +0600

    first commit

---
+++ Learn with Sumit/Class 12/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  perge: [],
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        mycolor: {
+          300: "#D5D5D5",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
